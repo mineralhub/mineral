1e243d3b1036a20f267afe0cbf4aa4bbdfaef5e1
ryoung-ho <chl4745@naver.com>
2018-07-20 16:14:11 +0900

    fix enable write console

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -38,6 +38,7 @@ namespace test
                 }
                 return;
                 */
+                Logger.WriteConsole = true;
                 Config.Initialize();
 
                 Initialize();
