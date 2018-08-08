ad16d80e8cdd7b44eaa4f0440865d900a6b073c6
RyoungHo <chl4745@naver.com>
2018-08-08 16:43:05 +0900

    remove. sql directory

--- Sky/Sql/Transaction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sky.Sql
-{
-    public static class Transaction
-    {
-        public static string SelectByBlockHash = @"SELECT ""hash"", ""version"", ""type"", ""from"", ""to"", ""timestamp"", ""amount"", ""fee"", ""data"" FROM transactions WHERE blockHash=@blockHash";
-        public static string InsertFormat = @"INSERT INTO transactions (""hash"", ""version"", ""from"", ""to"", ""timestamp"", ""amount"", ""fee"", ""blockHash"") VALUES " +
-            "(@hash, @version, @from, @to, @timestamp, @amount, @fee, @blockHash)";
-    }
-}
