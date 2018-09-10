199d2394427e10727b89de620f35e7a1c4dd3702
ryoung-ho <chl4745@naver.com>
2018-09-10 18:45:16 +0900

    add. UnitTests Project

---
+++ Sky.UnitTests/UT_Account.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sky.Wallets;
+using System.Text;
+
+namespace Sky.UnitTests
+{
+    [TestClass]
+    public class UT_Account
+    {
+        WalletAccount _account;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            byte[] pk = Encoding.Default.GetBytes("account");
+            _account = new WalletAccount(pk);
+        }
+
+        [TestMethod]
+        public void AddressLength()
+        {
+            _account.Address.Length.Should().Be(34);
+        }
+    }
+}
