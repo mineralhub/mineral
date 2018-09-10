bf87a135576458c8475cae977c99ebf5c99cfba6
ryoung-ho <chl4745@naver.com>
2018-09-10 18:55:28 +0900

    fix. address unit tests

--- Sky.UnitTests/UT_Account.cs
+++ Sky.UnitTests/UT_Account.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sky.Cryptography;
 using Sky.Wallets;
 using System.Text;
 
@@ -21,6 +22,24 @@ namespace Sky.UnitTests
         public void AddressLength()
         {
             _account.Address.Length.Should().Be(34);
+        }
+
+        [TestMethod]
+        public void AddressFormat()
+        {
+            _account.Address.Base58CheckDecode().Length.Should().Be(21);
+        }
+
+        [TestMethod]
+        public void HashToAddress()
+        {
+            WalletAccount.ToAddress(_account.AddressHash).Should().Be(_account.Address);
+        }
+
+        [TestMethod]
+        public void AddressToHash()
+        {
+            WalletAccount.ToAddressHash(_account.Address).Should().Be(_account.AddressHash);
         }
     }
 }
