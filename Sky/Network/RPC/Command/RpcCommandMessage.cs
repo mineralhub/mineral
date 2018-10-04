376bf67870691e846e86e74123f65e1e6cf891e3
franki22 <franki22@naver.com>
2018-10-04 17:12:25 +0900

    add RpcCommandMessage.cs

---
+++ Sky/Network/RPC/Command/RpcCommandMessage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace Sky.Network.RPC.Command
+{
+    public partial class RpcCommands
+    {
+        public delegate JObject ProcessHandler(object obj, JArray parameters);
+        //public delegate string InvalidParameterMessage();
+
+        //protected static Dictionary<object, InvalidParameterMessage> InvalidParameterHandlers = new Dictionary<object, InvalidParameterMessage>()
+        //{
+        //    { RpcCommands.Block.GetBlock, InvalidParameter_GetBlock },
+        //};
+
+        //public static string InvalidParameter_GetBlock()
+        //{
+        //    return "";
+        //}
+
+        //public static string GetInvalidParameterMessage(object obj)
+        //{
+        //    return InvalidParameterHandlers.ContainsKey(obj) ?
+        //        InvalidParameterHandlers[obj]() : "";
+        //}
+
+        public static JObject CreateErrorResponse(JToken id, int code, string message, string data = null)
+        {
+            JObject response = new JObject();
+            response["jsonrpc"] = "2.0";
+            response["id"] = id;
+            response["error"] = new JObject();
+            response["error"]["code"] = code;
+            response["error"]["message"] = message;
+            if (data != null)
+                response["error"]["data"] = data;
+            return response;
+        }
+    }
+}
