aa8a07a109e4e539b1a9671b6aa53423277fe7da
franki22 <franki22@naver.com>
2019-02-22 16:20:36 +0900

    Add RPC command description

---
+++ Mineral/CommandLine/Attributes/CommandLineAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mineral.CommandLine.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public sealed class CommandLineAttribute : Attribute, ICommandLineAttribute
+    {
+        private string name;
+        private string description;
+
+        public string Name { get { return this.name; } set { if (!this.name.Equals(value)) { this.name = value; } } }
+        public string Description { get { return this.description; } set { if (!this.description.Equals(value)) { this.description = value; } } }
+
+        public CommandLineAttribute()
+        {
+            this.name = string.Empty;
+            this.description = string.Empty;
+        }
+    }
+}
