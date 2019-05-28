5d298b2cd5672e49472f5e8ae0238d2ac30b57b5
franki22 <franki22@naver.com>
2019-05-28 16:56:49 +0900

    Add. ServiceContainer

---
+++ Mineral/Common/Application/IService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Core.Config.Arguments;
+
+namespace Mineral.Common.Application
+{
+    public interface IService
+    {
+        void Init();
+        void Init(Args args);
+        void Start();
+        void Stop();
+    }
+}
