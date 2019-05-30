2c7b4d04e21098536eb7443c0456ff11b5d7e14b
franki22 <franki22@naver.com>
2019-05-30 16:37:55 +0900

    Add Message class

---
+++ Mineral/Common/Overlay/Message/HelloMessage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Common.Overlay.Discover;
+
+namespace Mineral.Common.Overlay.Message
+{
+    public class HelloMessage : P2pMessage
+    {
+        #region Field
+        private Protocol.HelloMessage message = null;
+        #endregion
+
+
+        #region Property
+        #endregion
+
+
+        #region Constructor
+        public HelloMessage(byte type, byte[] raw_data)
+            : base(type, raw_data)
+        {
+            this.message = Protocol.HelloMessage.Parser.ParseFrom(raw_data);
+        }
+
+        public HelloMessage(Node node, long timestamp, BlockCapsule)
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        #endregion
+    }
+}
---
+++ Mineral/Common/Overlay/Message/MessageCodec.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotNetty.Buffers;
+using DotNetty.Codecs;
+using DotNetty.Transport.Channels;
+using Mineral.Common.Overlay.Server;
+
+namespace Mineral.Common.Overlay.Message
+{
+    public class MessageCodec : ByteToMessageDecoder
+    {
+        #region Field
+        private Channel channel;
+        private P2pMessageFactory p2p_message_factory = new P2pMessageFactory();
+        private MineralMessageFactory mineral_message_factory = new MineralMessageFactory();
+        #endregion
+
+
+        #region Property
+        #endregion
+
+
+        #region Constructor
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        protected override void Decode(IChannelHandlerContext context, IByteBuffer input, List<object> output)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
