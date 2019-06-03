90a025dc36504e75b73cfe8239d35db36309ab98
franki22 <franki22@naver.com>
2019-06-03 16:01:34 +0900

    Add TransactionResultCapsule class

---
+++ Mineral/Core/Capsule/TransactionResultCapsule.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Protobuf;
+
+namespace Mineral.Core.Capsule
+{
+    public class TransactionResultCapsule : IProtoCapsule<Protocol.Transaction.Types.Result>
+    {
+        #region Field
+        private Protocol.Transaction.Types.Result transaction_result = new Protocol.Transaction.Types.Result();
+        #endregion
+
+
+        #region Property
+        public Protocol.Transaction.Types.Result Instance { get { return this.transaction_result; } }
+        public byte[] Data { get { return this.transaction_result.ToByteArray(); } }
+
+        public long Fee
+        {
+            get { return this.transaction_result.Fee; }
+            set { this.transaction_result.Fee = value; }
+        }
+
+        public Protocol.Transaction.Types.Result.Types.code Result
+        {
+            get { return this.transaction_result.Ret; }
+            set { this.transaction_result.Ret = value; }
+        }
+
+        public long UnfreezeAmount
+        {
+            get { return this.transaction_result.UnfreezeAmount; }
+            set { this.transaction_result.UnfreezeAmount = value; }
+        }
+
+        public string AssetIssueID
+        {
+            get { return this.transaction_result.AssetIssueID; }
+            set { this.transaction_result.AssetIssueID = value; }
+        }
+
+        public long WithdrawAmount
+        {
+            get { return this.transaction_result.WithdrawAmount; }
+            set { this.transaction_result.WithdrawAmount = value; }
+        }
+
+        public long ExchangeReceivedAmount
+        {
+            get { return this.transaction_result.ExchangeReceivedAmount; }
+            set { this.transaction_result.ExchangeReceivedAmount = value; }
+        }
+
+        public long ExchangeWithdrawAnotherAmount
+        {
+            get { return this.transaction_result.ExchangeWithdrawAnotherAmount; }
+            set { this.transaction_result.ExchangeWithdrawAnotherAmount = value; }
+        }
+
+        public long ExchangeInjectAnotherAmount
+        {
+            get { return this.transaction_result.ExchangeInjectAnotherAmount; }
+            set { this.transaction_result.ExchangeInjectAnotherAmount = value; }
+        }
+
+        public long ExchangeId
+        {
+            get { return this.transaction_result.ExchangeId; }
+            set { this.transaction_result.ExchangeId = value; }
+        }
+        #endregion
+
+
+        #region Constructor
+        public TransactionResultCapsule(Protocol.Transaction.Types.Result transaction_result)
+        {
+            this.transaction_result = transaction_result;
+        }
+
+        public TransactionResultCapsule(byte[] data)
+        {
+            try
+            {
+                this.transaction_result = Protocol.Transaction.Types.Result.Parser.ParseFrom(data);
+            }
+            catch (System.Exception e)
+            {
+                Logger.Debug(e.Message);
+            }
+        }
+
+        public TransactionResultCapsule(Protocol.Transaction.Types.Result.Types.contractResult code)
+        {
+            this.transaction_result.ContractRet = code;
+        }
+
+        public TransactionResultCapsule(Protocol.Transaction.Types.Result.Types.code code, long fee)
+        {
+            this.transaction_result.Ret = code;
+            this.transaction_result.Fee = fee;
+        }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        public void AddFee(long fee)
+        {
+            this.transaction_result.Fee += fee;
+        }
+
+        public void SetStatus(long fee, Protocol.Transaction.Types.Result.Types.code code)
+        {
+            this.transaction_result.Fee += fee;
+            this.transaction_result.Ret = code;
+        }
+    }
+
+    #endregion
+}
+}
