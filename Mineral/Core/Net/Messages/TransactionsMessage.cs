1b23692f420987a87ad480e3fecf4b2c0993eb96
franki22 <franki22@naver.com>
2019-06-05 17:53:51 +0900

    Add TransactionsMessage class

---
+++ Mineral/Core/Net/Messages/TransactionsMessage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Protobuf;
+using Mineral.Core.Capsule;
+using Protocol;
+
+namespace Mineral.Core.Net.Messages
+{
+    public class TransactionsMessage : MineralMessage
+    {
+        #region Field
+        private Transactions transactions;
+        #endregion
+
+
+        #region Property
+        public Transactions Transactions => this.transactions;
+        #endregion
+
+
+        #region Constructor
+        public TransactionsMessage(List<Transaction> txs)
+        {
+            txs.ForEach(tx => this.transactions.Transactions_.Add(tx));
+            this.type = (byte)MessageTypes.MsgType.TRXS;
+            this.data = this.transactions.ToByteArray();
+        }
+
+        public TransactionsMessage(byte[] data)
+            : base(data)
+        {
+            this.type = (byte)MessageTypes.MsgType.TRXS;
+            this.transactions = Transactions.Parser.ParseFrom(data);
+            if (IsFilter)
+            {
+                CompareBytes(data, this.transactions.ToByteArray());
+                TransactionCapsule.ValidContractProto(new List<Transaction>(this.transactions.Transactions_));
+            }
+        }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(base.ToString())
+                   .Append("tx size : ")
+                   .Append(this.transactions.Transactions_.Count)
+
+            return builder.ToString();
+        }
+        #endregion
+    }
+}
