803719edb3ec46943b625aaa59e879ed7d1c201b
franki22 <franki22@naver.com>
2019-06-05 15:54:08 +0900

    Add MineralMessage class

---
+++ Mineral/Core/Net/Messages/MineralMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Common.Overlay.Messages;
+
+namespace Mineral.Core.Net.Messages
+{
+    public abstract class MineralMessage : Message
+    {
+        #region Field
+        #endregion
+
+
+        #region Property
+        #endregion
+
+
+        #region Constructor
+        public MineralMessage() { }
+        public MineralMessage(byte[] raw_data) : base(raw_data) { }
+        public MineralMessage(byte type, byte[] raw_data) : base(type, raw_data) { }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        #endregion
+    }
+}
