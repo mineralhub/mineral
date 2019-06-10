86678a561e723aa55c8dfa421d1f6f4bf1f66233
franki22 <franki22@naver.com>
2019-06-10 13:47:53 +0900

    Modify. BlockStore class

--- Mineral/Core/Database/BlockStore.cs
+++ Mineral/Core/Database/BlockStore.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Mineral.Common.Utils;
+using Mineral.Core.Capsule;
+using Protocol;
+using static Mineral.Core.Capsule.BlockCapsule;
 
 namespace Mineral.Core.Database
 {
-    public class BlockStore
+    public class BlockStore : MineralStoreWithRevoking<BlockCapsule, Block>
     {
         #region Field
         #endregion
@@ -15,6 +20,7 @@ namespace Mineral.Core.Database
 
 
         #region Constructor
+        public BlockStore(string db_name = "block") : base (db_name) { }
         #endregion
 
 
@@ -27,6 +33,36 @@ namespace Mineral.Core.Database
 
 
         #region External Method
+        public List<BlockCapsule> GetLimitNumber(long start, long limit)
+        {
+            BlockId id = new BlockId(SHA256Hash.ZERO_HASH, start);
+            return this.revoking_db.GetValuesNext(id.Hash, limit)
+                    .Select(data =>
+                    {
+                        try { return new BlockCapsule(data); }
+                        catch { }
+                        return null;
+                    })
+                    .ToArray()
+                    .Where(block => block != null)
+                    .OrderBy(block => block.Num)
+                    .ToList();
+        }
+
+        public List<BlockCapsule> GetBlockByLatestNum(long num)
+        {
+            return this.revoking_db.GetLatestValues(num)
+                    .Select(data =>
+                    {
+                        try { return new BlockCapsule(data); }
+                        catch { }
+                        return null;
+                    })
+                    .ToArray()
+                    .Where(block => block != null)
+                    .OrderBy(block => block.Num)
+                    .ToList();
+        }
         #endregion
     }
 }
