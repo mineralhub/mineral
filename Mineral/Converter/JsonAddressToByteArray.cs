32a42e6bc57f385095f886bd6f5a1f128c2d084e
franki <franki22@naver.com>
2019-06-12 16:26:56 +0900

    Add. JsonConverter wallet to byte array

---
+++ Mineral/Converter/JsonAddressToByteArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Core;
+using Newtonsoft.Json;
+
+namespace Mineral.Converter
+{
+    public class JsonAddressToByteArray : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(byte[]).Equals(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return Wallet.DecodeFromBase58Check(existingValue.ToString());
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(Wallet.Encode58Check((byte[])value));
+        }
+    }
+}
