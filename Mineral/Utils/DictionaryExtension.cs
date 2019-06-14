b2a07a6afcbcef64a8e074e1430ef21fb1c05b13
franki22 <franki22@naver.com>
2019-06-14 21:03:52 +0900

    Add. Dictionary util

---
+++ Mineral/Utils/DictionaryExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mineral.Utils
+{
+    public static class DictionaryExtension
+    {
+        public static void Put<Key, Value>(this Dictionary<Key, Value> dictionary, Key key, Value value)
+        {
+            if (dictionary.ContainsKey(key))
+                dictionary[key] = value;
+            else
+                dictionary.Add(key, value);
+        }
+
+        public static void Put<Key, Value>(this ConcurrentDictionary<Key, Value> dictionary, Key key, Value value)
+        {
+            if (dictionary.ContainsKey(key))
+                dictionary[key] = value;
+            else
+                dictionary.TryAdd(key, value);
+        }
+    }
+}
