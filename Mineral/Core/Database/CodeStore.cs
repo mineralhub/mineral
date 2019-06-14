5ae007904f917b04a95a0236ed38e62396792fd3
franki22 <franki22@naver.com>
2019-06-14 15:53:43 +0900

    Add. capsule and store

---
+++ Mineral/Core/Database/CodeStore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Core.Capsule;
+
+namespace Mineral.Core.Database
+{
+    public class CodeStore : MineralStoreWithRevoking<CodeCapsule, byte[]>
+    {
+        #region Field
+        #endregion
+
+
+        #region Property
+        #endregion
+
+
+        #region Contructor
+        public CodeStore(string db_name = "code") : base(db_name) { }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        public override CodeCapsule Get(byte[] key)
+        {
+            return GetUnchecked(key);
+        }
+        #endregion
+    }
+}
