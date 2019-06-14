88da6804f410bd2c9c5b6a89e0d945e8d5bff351
franki22 <franki22@naver.com>
2019-06-14 15:43:27 +0900

    Add. ProposalStore, StorageRowStore class

---
+++ Mineral/Core/Database/StorageRowStore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Core.Capsule;
+
+namespace Mineral.Core.Database
+{
+    public class StorageRowStore : MineralStoreWithRevoking<StorageRowCapsule, byte[]>
+    {
+        #region Field
+        #endregion
+
+
+        #region Property
+        #endregion
+
+
+        #region Contructor
+        public StorageRowStore(string db_name = "storage-row") : base(db_name) { }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        public override StorageRowCapsule Get(byte[] key)
+        {
+            return GetUnchecked(key);
+        }
+        #endregion
+    }
+}
