c1c7298215f5743a2ffdcc2b75f058ecd397a9c3
franki22 <franki22@naver.com>
2019-06-14 21:03:29 +0900

    Add. Database store class

---
+++ Mineral/Core/Database/ContractStore.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Core.Capsule;
+using Mineral.Utils;
+using Protocol;
+using static Protocol.Transaction.Types;
+
+namespace Mineral.Core.Database
+{
+    public class ContractStore : MineralStoreWithRevoking<ContractCapsule, Contract>
+    {
+        #region Field
+        #endregion
+
+
+        #region Property
+        #endregion
+
+
+        #region Contructor
+        public ContractStore(string db_name = "contract") : base(db_name) { }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        public override ContractCapsule Get(byte[] key)
+        {
+            return GetUnchecked(key);
+        }
+
+        public SmartContract.Types.ABI GetABI(byte[] contract_address)
+        {
+            byte[] value = this.revoking_db.GetUnchecked(contract_address);
+            if (value.IsNotNullOrEmpty())
+                return null;
+
+            ContractCapsule contract = new ContractCapsule(value);
+
+            return contract.Instance != null ? contract.Instance.Abi : null;
+        }
+        #endregion
+    }
+}
---
+++ Mineral/Core/Database/TransactionStore.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Common.Utils;
+using Mineral.Core.Capsule;
+using Mineral.Utils;
+using Protocol;
+
+namespace Mineral.Core.Database
+{
+    public class TransactionStore : MineralStoreWithRevoking<TransactionCapsule, Transaction>
+    {
+        #region Field
+        private BlockStore block_store;
+        private KhaosDatabase khaos_database;
+        #endregion
+
+
+        #region Property
+        #endregion
+
+
+        #region Contructor
+        public TransactionStore(BlockStore block, KhaosDatabase khaos, string db_name = "transaction")
+            : base(db_name)
+        {
+            this.block_store = block;
+            this.khaos_database = khaos;
+        }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        private TransactionCapsule GetTransactionFromBlockStore(byte[] key, long block_num)
+        {
+            List<BlockCapsule> blocks = this.block_store.GetLimitNumber(block_num, 1);
+            if (blocks.Count != 0)
+            {
+                foreach (TransactionCapsule tx in blocks[0].Transactions)
+                {
+                    if (tx.Id.Equals(SHA256Hash.Wrap(key)))
+                    {
+                        return tx;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private TransactionCapsule GetTransactionFromKhaosDatabase(byte[] key, long high)
+        {
+            List<KhaosBlock> blocks = this.khaos_database.MiniStore.GetBlockByNum(high);
+            foreach (KhaosBlock kblock in blocks)
+            {
+                foreach (TransactionCapsule tx in kblock.Block.Transactions)
+                {
+                    if (tx.Id.Equals(SHA256Hash.Wrap(key)))
+                    {
+                        return tx;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void DeleteIndex(byte[] key)
+        {
+            //if (Objects.nonNull(indexHelper))
+            //{
+            //    TransactionCapsule item;
+            //    try
+            //    {
+            //        item = get(key);
+            //        if (Objects.nonNull(item))
+            //        {
+            //            indexHelper.remove(item.getInstance());
+            //        }
+            //    }
+            //    catch (StoreException e)
+            //    {
+            //        logger.error("deleteIndex: ", e);
+            //    }
+            //}
+        }
+        #endregion
+
+
+        #region External Method
+        public override void Put(byte[] key, TransactionCapsule item)
+        {
+            if (item == null || item.BlockNum == -1)
+                base.Put(key, item);
+            else
+                this.revoking_db.Put(key, BitConverter.GetBytes(item.BlockNum));
+        }
+
+        public override TransactionCapsule Get(byte[] key)
+        {
+            TransactionCapsule transaction = null;
+            byte[] value = this.revoking_db.GetUnchecked(key);
+
+            if (value.IsNotNullOrEmpty())
+            {
+                if (value.Length == 8)
+                {
+                    long block_num = BitConverter.ToInt64(value, 0);
+                    transaction = GetTransactionFromBlockStore(key, block_num);
+                    if (transaction == null)
+                    {
+                        transaction = GetTransactionFromKhaosDatabase(key, block_num);
+                    }
+                }
+
+                transaction = transaction ?? new TransactionCapsule(value);
+            }
+
+            return transaction;
+        }
+
+        public override TransactionCapsule GetUnchecked(byte[] key)
+        {
+            TransactionCapsule transaction = null;
+            try
+            {
+                transaction = Get(key);
+            }
+            catch
+            {
+                transaction = null;
+            }
+
+            return transaction;
+        }
+
+        public override void Delete(byte[] key)
+        {
+            DeleteIndex(key);
+            base.Delete(key);
+        }
+        #endregion
+    }
+}
