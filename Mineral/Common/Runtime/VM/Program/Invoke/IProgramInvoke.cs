d3854034853cbc75ff81a37fbc7593031910f193
franki22 <franki22@naver.com>
2019-06-17 16:36:14 +0900

    Add. Invoke program interface

---
+++ Mineral/Common/Runtime/VM/Program/Invoke/IProgramInvoke.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Common.Storage;
+using Mineral.Core.Capsule;
+
+namespace Mineral.Common.Runtime.VM.Program.Invoke
+{
+    public interface IProgramInvoke
+    {
+        bool IsStaticCall { get; }
+
+        DataWord GetContractAddress();
+        DataWord GetBalance();
+        DataWord GetOriginAddress();
+        DataWord GetCallerAddress();
+        DataWord GetCallValue();
+        DataWord GetTokenValue();
+        DataWord GetTokenId();
+        DataWord GetDataSize();
+        DataWord GetDataValue(DataWord index_data);
+        DataWord GetPrevHash();
+        DataWord GetCoinbase();
+        DataWord GetTimestamp();
+        DataWord GetNumber();
+        DataWord GetDifficulty();
+        BlockCapsule GetBlockByNum(int index);
+        Deposit GetDeposit();
+
+        byte[] GetDataCopy(DataWord offset_data, DataWord length_data);
+        bool ByTestingSuite();
+        int GetCallDeep();
+        long GetVMShouldEndInUs();
+        long GetVMStartInUs();
+        long GetEnergyLimit();
+        void GetStaticCall();
+    }
+}
---
+++ Mineral/Common/Runtime/VM/Program/Invoke/IProgramInvokeFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Common.Storage;
+using Protocol;
+
+namespace Mineral.Common.Runtime.VM.Program.Invoke
+{
+    public interface IProgramInvokeFactory
+    {
+        IProgramInvoke CreateProgramInvoke(
+                    InternalTransaction.TransactionType tx_type,
+                    InternalTransaction.ExecutorType executor_type,
+                    Transaction tx,
+                    long token_value,
+                    long token_id,
+                    Block block,
+                    Deposit deposit,
+                    long vm_start_us,
+                    long vm_should_end_us,
+                    long energy_limit);
+
+        IProgramInvoke CreateProgramInvoke(
+                    Program program,
+                    DataWord to_adderess,
+                    DataWord caller_address,
+                    DataWord in_value,
+                    DataWord token_value,
+                    DataWord token_id,
+                    long balance,
+                    byte[] data,
+                    Deposit deposit,
+                    bool static_call,
+                    bool  by_testing_suite,
+                    long vm_start_us,
+                    long vm_should_end_us,
+                    long energy_limit)
+    }
+}
