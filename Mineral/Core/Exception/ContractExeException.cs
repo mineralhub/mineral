c819196b017779a4f6abefce2c553a982572ac9c
franki22 <franki22@naver.com>
2019-06-18 17:09:21 +0900

    Add ContractExeException exception

---
+++ Mineral/Core/Exception/ContractExeException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mineral.Core.Exception
+{
+    [Serializable]
+    public class ContractExeException : System.Exception
+    {
+        public ContractExeException() { }
+        public ContractExeException(string message) : base(message) { }
+        public ContractExeException(string message, System.Exception inner) : base(message, inner) { }
+        protected ContractExeException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
