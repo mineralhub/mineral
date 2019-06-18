29aefa8bc5a2dabf18d5e65e975e32699c604449
franki22 <franki22@naver.com>
2019-06-18 16:28:55 +0900

    Add Actuator base class and interface

---
+++ Mineral/Core/Actuator/IActuator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Protobuf;
+using Mineral.Core.Capsule;
+
+namespace Mineral.Core.Actuator
+{
+    public interface IActuator
+    {
+        bool Execute(TransactionResultCapsule result);
+        bool Validate();
+        long CalcFee();
+        ByteString GetOwnerAddress();
+    }
+}
