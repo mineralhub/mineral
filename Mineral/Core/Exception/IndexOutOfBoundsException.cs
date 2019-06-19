0fc19e5757cd237147ac0d247eb9b38258918bab
franki22 <franki22@naver.com>
2019-06-19 20:15:14 +0900

    Add Exceptions

---
+++ Mineral/Core/Exception/IndexOutOfBoundsException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mineral.Core.Exception
+{
+    [Serializable]
+    public class IndexOutOfBoundsException : System.Exception
+    {
+        public IndexOutOfBoundsException() { }
+        public IndexOutOfBoundsException(string message) : base(message) { }
+        public IndexOutOfBoundsException(string message, System.Exception inner) : base(message, inner) { }
+        protected IndexOutOfBoundsException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
