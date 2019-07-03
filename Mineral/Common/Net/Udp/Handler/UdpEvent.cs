d5899abc6e51a011685ea78f42933e400cc7160b
franki22 <franki22@naver.com>
2019-07-03 18:57:19 +0900

    Add. Net-UDP classes

---
+++ Mineral/Common/Net/Udp/Handler/UdpEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mineral.Common.Net.Udp.Handler
+{
+    public class UdpEvent
+    {
+        #region Field
+        #endregion
+
+
+        #region Property
+        public Message.Message Message { get; set; }
+        public System.Net.IPEndPoint Address { get; set; }
+        #endregion
+
+
+        #region Contructor
+        public UdpEvent(Message.Message message, System.Net.IPEndPoint address)
+        {
+            this.Message = message;
+            this.Address = address;
+        }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        #endregion
+    }
+}
---
+++ Mineral/Common/Net/Udp/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Common.Net.Udp.Handler;
+
+namespace Mineral.Common.Net.Udp
+{
+    public interface IEventHandler
+    {
+        void ChannelActivated();
+        void HandlerEvent(UdpEvent udp_event);
+    }
+}
