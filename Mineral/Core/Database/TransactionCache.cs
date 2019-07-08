79f81946dce56d3155e5d2b4187bedead0b52f48
franki22 <franki22@naver.com>
2019-07-08 17:19:23 +0900

    Add TransactionCache class

---
+++ Mineral/Core/Database/TransactionCache.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Core.Capsule;
+
+namespace Mineral.Core.Database
+{
+    public class TransactionCache : MineralStoreWithRevoking<BytesCapsule, object>
+    {
+        #region Field
+        #endregion
+
+
+        #region Property
+        #endregion
+
+
+        #region Contructor
+        public TransactionCache(string db_name = "trans-cache") : base(db_name) { }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        #endregion
+    }
+}
