364825d92809a143c6b022b6e5513c560c10bd86
franki22 <franki22@naver.com>
2019-07-11 19:37:28 +0900

    Add interface IDiscoverListener

---
+++ Mineral/Common/Overlay/Discover/IDiscoverListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mineral.Common.Overlay.Discover.Node;
+
+namespace Mineral.Common.Overlay.Discover
+{
+    public interface IDiscoverListener
+    {
+        void NodeAppeared(NodeHandler handler);
+        void NodeDisappeared(NodeHandler handler);
+    }
+}
