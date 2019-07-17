dbd4b5695acb74b6d746df2396dde08e80cd3082
franki22 <franki22@naver.com>
2019-07-17 19:38:06 +0900

    Add Runnable class

---
+++ Mineral/Utils/Runnable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Mineral.Utils
+{
+    public abstract class Runnable
+    {
+        #region Field
+        #endregion
+
+
+        #region Property
+        #endregion
+
+
+        #region Contructor
+        public Runnable() { }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        public abstract void Run();
+        #endregion
+
+
+        #region External Method
+        #endregion
+    }
+}
