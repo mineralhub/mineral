4d5365e49ee613b7d4cc6b9ccd86b61d0045dcfb
franki <franki22@naver.com>
2019-08-08 21:55:41 +0900

    Add cache library

---
+++ Mineral/Core/Cache/CacheUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mineral.Core.Cache
+{
+    public static class CacheUtil
+    {
+        public static long SaturatedAdd(long value1, long value2)
+        {
+            long naive_sum = value1 + value2;
+
+            if ((value1 ^ value2) < 0 | (value1 ^ naive_sum) >= 0)
+            {
+                return naive_sum;
+            }
+
+            return long.MaxValue + (((long)((ulong)naive_sum >> (sizeof(long) - 1))) ^ 1);
+        }
+
+        public static long SaturatedSubtract(long value1, long value2)
+        {
+            long diff = value1 - value2;
+            if ((value1 ^ value2) >= 0 | (value1 ^ diff) >= 0)
+            {
+                return diff;
+            }
+
+            return long.MaxValue + ((long)((ulong)(diff >> (sizeof(long) - 1))) ^ 1);
+        }
+
+    }
+}
---
+++ Mineral/Core/Cache/IWeigher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mineral.Core.Cache
+{
+    public interface IWeigher<TKey, TValue>
+    {
+        int Weigh(TKey key, TValue value);
+    }
+}
