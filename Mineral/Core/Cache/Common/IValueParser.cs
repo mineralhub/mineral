31699d60df63de8058948547cd6f162c5cd048e5
franki <franki22@naver.com>
2019-08-09 18:08:16 +0900

    Add cache libarary

---
+++ Mineral/Core/Cache/Common/IValueParser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mineral.Core.Cache.Common
+{
+    internal interface IValueParser
+    {
+        void Parse(CacheBuilderSpec spec, string key, string value);
+    }
+}
---
+++ Mineral/Core/Cache/DefineCache.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mineral.Core.Cache
+{
+    public enum Strength
+    {
+        STRONG,
+        SOFT,
+        WEAK
+    }
+
+    public enum RemovalCause
+    {
+        EXPLICIT,
+        REPLACED,
+        COLLECTED,
+        EXPIRED,
+        SIZE
+    }
+
+    public enum TimeUnit
+    {
+        MICROSECONDS,
+        MILLISECONDS,
+        SECONDS,
+        MINUTES,
+        HOURS,
+        DAYS,
+        YEARS
+    }
+}
