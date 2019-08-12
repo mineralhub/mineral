22229eaf3a7599b6d10f9a1e7b144bda9283d643
franki22 <franki22@naver.com>
2019-08-13 01:14:19 +0900

    Add cache reference

---
+++ Mineral/Core/Cache/Reference/IValueReference.cs
@@ -0,0 +1,22 @@
+﻿using Mineral.Core.Cache.Entry;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Mineral.Core.Cache
+{
+    public interface IValueReference<TKey, TValue>
+    {
+        IReferenceEntry<TKey, TValue> Entry { get; }
+        int Weight { get; }
+        bool IsLoading { get; }
+        bool IsActive { get; }
+
+        TValue Get();
+        TValue WaitForValue();
+
+        IValueReference<TKey, TValue> Copy(Queue<TValue> queue, TValue value, IReferenceEntry<TKey, TValue> entry);
+        void NotifyNewValue(TValue value);
+    }
+}
---
+++ Mineral/Core/Cache/Reference/UnsetValueReference.cs
@@ -0,0 +1,71 @@
+﻿using Mineral.Core.Cache.Entry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mineral.Core.Cache.Reference
+{
+    public class UnsetValueReference<TKey, TValue> : IValueReference<TKey, TValue>
+    {
+        #region Field
+        #endregion
+
+
+        #region Property
+        public IReferenceEntry<TKey, TValue> Entry
+        {
+            get { return null; }
+        }
+
+        public int Weight
+        {
+            get { return 0; }
+        }
+
+        public bool IsLoading
+        {
+            get { return false; }
+        }
+
+        public bool IsActive
+        {
+            get { return false; }
+        }
+
+        public IValueReference<TKey, TValue> Copy(Queue<TValue> queue, TValue value, IReferenceEntry<TKey, TValue> entry)
+        {
+            return this;
+        }
+
+        public TValue Get()
+        {
+            return default(TValue);
+        }
+
+        public void NotifyNewValue(TValue value)
+        {
+        }
+
+        public TValue WaitForValue()
+        {
+            return default(TValue);
+        }
+        #endregion
+
+
+        #region Constructor
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        #endregion
+    }
+}
