8ec97cfe3e873211ed23240c670931cd66bdf29e
franki22 <franki22@naver.com>
2019-08-12 18:44:55 +0900

    Add cache libarary

---
+++ Mineral/Core/Cache/ILoadingCache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mineral.Core.Cache
+{
+    public interface ILoadingCache<TKey, TValue> : ICache<TKey, TValue>
+    {
+        TValue GetUnchecked(TKey key);
+        TValue Apply(TKey key);
+        void Refresh(TKey key);
+    }
+}
