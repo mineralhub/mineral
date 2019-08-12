e9d78ccec2ddf79d36597dbf453a37ee9cf1dc9c
franki22 <franki22@naver.com>
2019-08-12 21:04:31 +0900

    Add cache libarary

---
+++ Mineral/Core/Cache/CacheLoader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mineral.Core.Cache
+{
+    public abstract class CacheLoader<TKey, TValue>
+    {
+        #region Field
+        #endregion
+
+
+        #region Property
+        #endregion
+
+
+        #region Constructor
+        protected CacheLoader() { }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        public abstract TValue Load(TKey key);
+
+        public Task<TValue> Relaod(TKey key, TValue old_value)
+        {
+            if (key == null)
+                throw new ArgumentNullException("Reload key is null");
+
+            if (old_value == null)
+                throw new ArgumentNullException("Reload old value is null");
+
+            return Task.Run<TValue>(() =>
+            {
+                return Load(key);
+            });
+        }
+
+        public static CacheLoader<TKey, TValue> From(Func<TKey, TValue> function)
+        {
+            return new FunctionToCacheLoader<TKey, TValue>(function);
+        }
+        #endregion
+    }
+}
---
+++ Mineral/Core/Cache/FunctionToCacheLoader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mineral.Core.Cache
+{
+    public class FunctionToCacheLoader<TKey, TValue> : CacheLoader<TKey, TValue>
+    {
+        #region Field
+        private Func<TKey, TValue> func_compute;
+        #endregion
+
+
+        #region Property
+        #endregion
+
+
+        #region Constructor
+        public FunctionToCacheLoader(Func<TKey, TValue> function)
+        {
+            this.func_compute = function ?? throw new ArgumentNullException();
+        }
+        #endregion
+
+
+        #region Event Method
+        #endregion
+
+
+        #region Internal Method
+        #endregion
+
+
+        #region External Method
+        public override TValue Load(TKey key)
+        {
+            return func_compute(key);
+        }
+        #endregion
+    }
+}
