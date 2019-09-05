a3c4cb2be084a8345e2db4c55dcf6f904168ec89
franki <franki22@naver.com>
2019-09-05 22:16:45 +0900

    Add CancelException

---
+++ MineralCLI/Exception/CancelException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineralCLI.Exception
+{
+    [Serializable]
+    public class CancelException : System.Exception
+    {
+        public CancelException() { }
+        public CancelException(string message) : base(message) { }
+        public CancelException(string message, System.Exception inner) : base(message, inner) { }
+        protected CancelException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
