8807ce7ece383826cb9d14cce5391577a31b2245
franki22 <franki22@naver.com>
2019-10-28 18:16:32 +0900

    Add converter bytes list

---
+++ Mineral/Converter/JsonListByteArrayConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Mineral.Converter
+{
+    public class JsonListByteArrayConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(byte[]).Equals(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            List<byte[]> result = new List<byte[]>();
+            JToken token = JToken.Load(reader);
+
+            foreach (string item in token)
+            {
+                result.Add(item.HexToBytes());
+            }
+
+            return result;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            List<byte[]> items = value as List<byte[]>;
+            List<string> result = items.Select(bytes =>
+            {
+                return bytes.ToHexString();
+            }).ToList();
+
+            serializer.Serialize(writer, result);
+        }
+    }
+}
