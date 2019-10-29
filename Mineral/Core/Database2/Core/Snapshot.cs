4e010482501f56046a9596653f4b0edd2cc658fa
franki22 <franki22@naver.com>
2019-10-29 18:35:29 +0900

    bug fix

--- Mineral/Core/Database2/Core/Snapshot.cs
+++ Mineral/Core/Database2/Core/Snapshot.cs
@@ -166,7 +166,7 @@ namespace Mineral.Core.Database2.Core
 
            return Enumerable.Concat(
                 Enumerable.Select(all, val => new KeyValuePair<byte[], byte[]>(val.Key.Data, val.Value.Data)),
-                Enumerable.Where(GetRoot(), val => keys.Contains(WrappedByteArray.Of(val.Key)))
+                Enumerable.Where(GetRoot(), val => !keys.Contains(WrappedByteArray.Of(val.Key)))
                 ).GetEnumerator();
         }
         #endregion
