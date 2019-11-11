0fee158c7a93cbee080d1f45185607ba4db20cd9
franki22 <franki22@naver.com>
2019-11-11 20:25:51 +0900

    bug fix. #50 Issue, CLI command GetBlockByLatestNum is invalid data

--- Mineral/Core/Database2/Common/IRevokingDB.cs
+++ Mineral/Core/Database2/Common/IRevokingDB.cs
@@ -7,9 +7,9 @@ namespace Mineral.Core.Database2.Common
     public interface IRevokingDB : IDatabase<byte[]>, IEnumerable<KeyValuePair<byte[], byte[]>>
     {
         void SetMode(bool mode);
-        HashSet<byte[]> GetLatestValues(long limit);
-        HashSet<byte[]> GetValuesNext(byte[] key, long limit);
-        HashSet<byte[]> GetValuesPrevious(byte[] key, long limit);
+        List<byte[]> GetLatestValues(long limit);
+        List<byte[]> GetValuesNext(byte[] key, long limit);
+        List<byte[]> GetValuesPrevious(byte[] key, long limit);
         Dictionary<byte[], byte[]> GetAllValues();
     }
 }
