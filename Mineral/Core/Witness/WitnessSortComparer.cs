78edd2c87a6a2a173042e44b5e0e11f887ea42f5
franki22 <franki22@naver.com>
2019-11-11 20:02:44 +0900

    modify. Sort descending witness

--- Mineral/Core/Witness/WitnessSortComparer.cs
+++ Mineral/Core/Witness/WitnessSortComparer.cs
@@ -7,27 +7,28 @@ namespace Mineral.Core.Witness
 {
     public class WitnessSortComparer : IComparer<WitnessCapsule>
     {
+        // Descending
         public int Compare(WitnessCapsule witness1, WitnessCapsule witness2)
         {
             int result = 0;
 
             if (witness1.VoteCount > witness2.VoteCount)
             {
-                result = 1;
+                result = -1;
             }
             else if (witness1.VoteCount < witness2.VoteCount)
             {
-                result = -1;
+                result = 1;
             }
             else
             {
                 if (witness1.GetHashCode() > witness2.GetHashCode())
                 {
-                    result = 1;
+                    result = -1;
                 }
                 else if (witness1.GetHashCode() < witness2.GetHashCode())
                 {
-                    result = -1;
+                    result = 1;
                 }
                 else
                 {
