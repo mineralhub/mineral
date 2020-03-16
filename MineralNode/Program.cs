345247c6217a19438f5f3cfa091108a6dd01e013
franki22 <franki22@naver.com>
2020-03-16 19:01:03 +0900

    Add program close event

--- MineralNode/Program.cs
+++ MineralNode/Program.cs
@@ -11,28 +11,19 @@ namespace MineralNode
 {
     class Program
     {
-        static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            Exception ex = (Exception)e.ExceptionObject;
-            StringBuilder builder = new StringBuilder();
-            builder.AppendLine(ex.GetType().ToString());
-            builder.AppendLine(ex.Message);
-            builder.AppendLine(ex.StackTrace);
-            builder.AppendLine();
-            File.AppendAllText("./error-log", builder.ToString());
-        }
+        private static readonly Application app = new Application();
 
         static void Main(string[] args)
         {
             AppDomain.CurrentDomain.UnhandledException += UnhandledException;
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+            Console.CancelKeyPress += Console_CancelKeyPress;
 
             Logger.Info("Mineral full-node Start...........");
 
             if (Args.Instance.SetParam(args, DefineParameter.CONF_FILE))
             {
                 Manager.Instance.Init();
-
-                Application app = new Application();
 
                 if (Args.Instance.IsWitness)
                     app.AddService(new WitnessService(Manager.Instance));
@@ -44,6 +35,34 @@ namespace MineralNode
                 app.StartService();
                 app.Startup();
             }
+        }
+
+        static void Close()
+        {
+            Logger.Info("Mineral full-node closing...........");
+            app.ShutdownService();
+            app.Shutdown();
+        }
+
+        static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = (Exception)e.ExceptionObject;
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine(ex.GetType().ToString());
+            builder.AppendLine(ex.Message);
+            builder.AppendLine(ex.StackTrace);
+            builder.AppendLine();
+            File.AppendAllText("./error-log", builder.ToString());
+        }
+
+        static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Close();
         }
     }
 }
